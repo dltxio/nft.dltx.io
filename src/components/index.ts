5cbaaeb9f010373def4645f624b260ea53e8e4a9
brettvniekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-08-30 19:51:02 +1000

    Rebased

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,4 @@ export { default as Mesh } from "./Mesh";
 export { default as MeshMember } from "./MeshMember";
 export { default as Modal } from "./Modal";
 export { default as MeshModal } from "./MeshModal";
+export { default as Logo } from "./Logo";
---
+++ src/pages/Admin.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { PageLayout, Logo } from "../components";
+
+const Admin: React.FC = () => {
+  return (
+    <PageLayout>
+      <Logo />
+    </PageLayout>
+  );
+};
+
+export default Admin;
