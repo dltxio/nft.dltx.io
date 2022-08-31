6eaa4f2a46d768f9296e039915dff713ee3a1ad3
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-08-31 10:31:51 +1000

    Added Mesh contract abi and integration

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { PageLayout, Logo } from "../components";
+import { Mesh } from "../contracts/Mesh";
+import { Mesh__factory } from "../contracts/factories";
+import { useWagmi } from "../hooks";
 
 const Admin: React.FC = () => {
+  const wagmi = useWagmi();
+  const [meshie, setMeshie] = useState<Mesh>();
+
+  useEffect(() => {
+    if (!wagmi.signer) return;
+
+    const meshieContract = Mesh__factory.connect(
+      process.env.VITE_MESHIE_ADDRESS as string,
+      wagmi.signer
+    );
+    setMeshie(meshieContract);
+  }, [wagmi.signer]);
+
   return (
     <PageLayout>
       <Logo />
+      {meshie ? <div>Connected</div> : <span>Loading...</span>}
     </PageLayout>
   );
 };
