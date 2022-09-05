0574f2e97ca395cdd75460e8a515af7c938b1799
Brett van Niekerk <79682833+brettvniekerk@users.noreply.github.com>
2022-09-05 13:51:12 +1000

    Moved wagmi logic in Dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,41 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { PageLayout, Logo, ContractPanel } from "../components";
-import { Mesh } from "../contracts/Mesh";
 import { Mesh__factory } from "../contracts/factories";
 import { useWagmi } from "../hooks";
 
 const Dashboard: React.FC = () => {
   const wagmi = useWagmi();
-  const [meshie, setMeshie] = useState<Mesh>();
-  const [address, setAddress] = useState<string>();
 
-  useEffect(() => {
-    if (!wagmi.signer || !wagmi.address) return;
-
-    if (!process.env.VITE_PROXY_ADDRESS)
-      throw new Error("cannot find environment variable VITE_PROXY_ADDRESS");
-
-    const meshieContract = Mesh__factory.connect(
-      process.env.VITE_PROXY_ADDRESS,
-      wagmi.signer
-    );
-
-    setMeshie(meshieContract);
-    setAddress(wagmi.address);
-  }, [wagmi.signer]);
+  if (!process.env.VITE_PROXY_ADDRESS)
+    throw new Error("cannot find environment variable VITE_PROXY_ADDRESS");
 
   return (
     <PageLayout>
       <Logo />
-      {meshie && address ? (
+      {wagmi.signer && wagmi.address ? (
         <div className="w-3/4 bg-black border-[1.5px] border-white/20 rounded-xl p-4 flex flex-col justify-center items-center box-border">
           <h1 className="w-[95%] text-center text-2xl font-black border-b-[1.5px] border-white/20 pb-4">
             {">>>>> Meshie Contract Dashboard <<<<<"}
           </h1>
-          <ContractPanel contract={meshie} address={address} />
+          <ContractPanel
+            contract={Mesh__factory.connect(
+              process.env.VITE_PROXY_ADDRESS,
+              wagmi.signer
+            )}
+            address={wagmi.address}
+          />
         </div>
       ) : (
-        <span className="text-xl">Loading...</span>
+        <span className="text-xl">Please connect wallet</span>
       )}
     </PageLayout>
   );
